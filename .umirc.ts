112ea67f7eba0d7dafcebfffd19f20697dbce9f8
hstarorg <hm910705@163.com>
2021-12-12 22:49:47 +0800

    docs: use site mode

--- .umirc.ts
+++ .umirc.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'dumi';
 // more config: https://d.umijs.org/config
 export default defineConfig({
   title: 'Bizify',
+  mode: 'site',
   favicon: 'https://cdn1.hstar.vip/logo.png',
   logo: 'https://cdn1.hstar.vip/logo.png',
   outputPath: 'docs-dist',
