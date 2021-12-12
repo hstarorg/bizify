96f63d379e601db18490b0d3f00332e243b76fc5
hstarorg <hm910705@163.com>
2021-12-12 19:51:53 +0800

    chore: root commands update

--- .umirc.ts
+++ .umirc.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'dumi';
 
+// more config: https://d.umijs.org/config
 export default defineConfig({
-  title: ' ',
+  title: 'Bizify',
   favicon: 'https://cdn1.hstar.vip/logo.png',
   logo: 'https://cdn1.hstar.vip/logo.png',
   outputPath: 'docs-dist',
-  // more config: https://d.umijs.org/config
 });
--- docs/idea/page-state/index.md
+++ docs/idea/page-state/index.md
@@ -2,6 +2,6 @@
 group:
   path: idea
   title: 理念
-title: 页面级状态 VS. 全局状态
+title: 页面自治 VS. 全局状态
 order: 10
 ---
